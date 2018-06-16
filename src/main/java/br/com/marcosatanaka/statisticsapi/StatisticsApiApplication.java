13dc8cb46c3afab9c157101803f7e2d8b1ea764b
Marcos Tanaka <marcosatanaka@gmail.com>
2018-06-16 10:24:28 -0300

    Initial project

---
+++ src/main/java/br/com/marcosatanaka/statisticsapi/StatisticsApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.marcosatanaka.statisticsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticsApiApplication.class, args);
+	}
+}
